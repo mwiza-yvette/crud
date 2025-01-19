385bcd377ee10dbb76b3376fa91dedcab59188c3
mwiza yvette <mwizamarieyvette@gmail.com>
2025-01-19 20:41:55 +0200

    initial commit

---
+++ insert_data.php
@@ -0,0 +1,33 @@
+<?php include('dbcon.php') ?>
+<?php
+
+if(isset($_POST['add_students'])){
+    $fname = $_POST['f_name'];
+    $lname = $_POST['l_name'];
+    $age = $_POST['age'];
+
+    if($fname == "" || empty($fname)){
+        header('location:indeex.php?message=you have to fill firstName!');
+    }
+    elseif($lname == "" || empty($lname)){
+        header('location:indeex.php?message=you have to fill lastname!');
+
+    }
+    elseif($age == "" || empty($age)){
+        header('location:indeex.php?message=you have to fill age!');
+
+    }
+else{
+    $query = "INSERT INTO inputs(firstname,lastname,age) values('$fname','$lname','$age')";
+    $result =mysqli_query($connection,$query);   
+    if(!$result){
+        die("query failed".mysqli_error());
+    }
+    else{
+        header('location:indeex.php?insert_sms=your data have been inserted successfuly');
+    }
+}
+}
+
+
+?>
